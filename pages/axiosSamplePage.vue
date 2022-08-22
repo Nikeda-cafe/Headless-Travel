8f83012df47502da79a628dc1a83e5143dedd286
nikeda <4104cafeaulait@gmail.com>
2022-08-23 02:24:58 +0900

    card

--- pages/axiosSamplePage.vue
+++ pages/axiosSamplePage.vue
@@ -1,9 +1,6 @@
 <template>
-  <div>
+  <div class="lg:w-3/4 m-auto">
       <h1 class="text-center mb-3">記事を取得</h1>
-      <p>{{res.body}}</p>
-      <img :src="res.image.url" alt="">
-      <!-- <li>{{menuItems[0].body}}</li> -->
       <div class="input-group w-3/4 m-auto">
         <div class="input-group-prepend">
           <span class="input-group-text">MenuId</span>
@@ -20,34 +17,38 @@
         </button>
       </div>
 
+      <!-- news card -->
+      <div class="post__list text-center">
+        <ul class="flex justify-between flex-wrap w-10/12 m-auto">
+          <li v-for="(item,index) in posts.contents" :key="index" class="mb-4">
+            <div class="overflow-hidden shadow-lg rounded-lg h-90 lg:w-72 md:w-80 cursor-pointer m-auto">
+                <a href="#" class="w-full block h-full">
+                    <img alt="blog photo" :src="item.thumbnail.url" class="max-h-40 w-full object-cover"/>
+                    <div class="bg-white dark:bg-gray-800 w-full p-4">
+                        <p class="text-indigo-500 text-md font-medium">
+                        </p>
+                        <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+                            {{item.title}}
+                        </p>
+                        <p class="text-gray-400 dark:text-gray-300 font-light text-md" v-html="trimPostBody(item.body)">
+
+                        </p>
+                    </div>
+                </a>
+            </div>
+          </li>
+        </ul>
+      </div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios'
 export default {
-  // data: function(){
-  //   return {
-  //     res:'',
-  //     id: this.$route.params.id
-  //   }
-  // },
-  // methods: {
-  //   async getAxios(){
-  //     const res = await this.$axios.$get('https://api.coindesk.com/v1/bpi/currentprice.json')
-  //     console.log(res);
-  //     this.res = res.time.updated;
-  //   }
-  // }
 
   data: function(){
     return {
-      res:{
-        body:'',
-        image:{
-          url:''
-        }
-      },
+      // posts: {},
       id: this.$route.params.id,
       menuId:'',
       src: ''
@@ -59,18 +60,29 @@ export default {
         path: "menu-detail",
         query: { menuId: this.menuId }
       });
+    },
+    trimPostBody(body){
+      const trimBody = body.slice(0,50) + '...';
+      return trimBody
     }
   },
 
   async asyncData({ query,$config,$axios }){
     const result = await $axios.$get(
-      `${$config.apiUrl}/menu/`,
+      `${$config.apiUrl}/news/`,
       {
         headers: {'X-API-KEY': '691867be-4a35-4006-90c1-9b0856070900'},
       }
     )
-    return console.log(result)
+    return {
+      posts : result
+    }
 
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
+
