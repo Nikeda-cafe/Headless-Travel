5cdcee7cb0dda36a92c87fc0603879150108f61e
nikeda <4104cafeaulait@gmail.com>
2022-08-23 21:59:26 +0900

    コンポーネント化

---
+++ components/NewsList.vue
@@ -0,0 +1,43 @@
+<template lang="">
+    <li class="mb-4">
+        <div class="overflow-hidden shadow-lg rounded-lg h-90 lg:w-72 md:w-80 cursor-pointer m-auto">
+            <a href="#" class="w-full block h-full">
+                <img alt="blog photo" :src="url" class="max-h-40 w-full object-cover"/>
+                <div class="bg-white dark:bg-gray-800 w-full p-4">
+                    <p class="text-indigo-500 text-md font-medium"></p>
+                    <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
+                        {{title}}
+                    </p>
+                    <p class="text-gray-400 dark:text-gray-300 font-light text-md" v-html="trimPostBody(body)"></p>
+                </div>
+            </a>
+        </div>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'NewsList',
+  props: {
+    url: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true
+    }
+  },
+  methods: {
+    trimPostBody(body){
+      const trimBody = body.slice(0,50) + '...';
+      return trimBody
+    }
+  },
+}
+
+</script>
--- pages/axiosSamplePage.vue
+++ pages/axiosSamplePage.vue
@@ -20,65 +20,46 @@
       <!-- news card -->
       <div class="post__list text-center">
         <ul class="flex justify-between flex-wrap w-10/12 lg:w-full m-auto">
-          <li v-for="(item,index) in posts.contents" :key="index" class="mb-4">
-            <div class="overflow-hidden shadow-lg rounded-lg h-90 lg:w-72 md:w-80 cursor-pointer m-auto">
-                <a href="#" class="w-full block h-full">
-                    <img alt="blog photo" :src="item.thumbnail.url" class="max-h-40 w-full object-cover"/>
-                    <div class="bg-white dark:bg-gray-800 w-full p-4">
-                        <p class="text-indigo-500 text-md font-medium">
-                        </p>
-                        <p class="text-gray-800 dark:text-white text-xl font-medium mb-2">
-                            {{item.title}}
-                        </p>
-                        <p class="text-gray-400 dark:text-gray-300 font-light text-md" v-html="trimPostBody(item.body)">
-
-                        </p>
-                    </div>
-                </a>
-            </div>
-          </li>
+            <NewsList
+                v-for="(item,index) in posts.contents"
+                :key="index" :url="item.thumbnail.url"
+                :body="item.body"
+                :title="item.title"
+            />
         </ul>
       </div>
   </div>
 </template>
 
 <script>
+import NewsList from '../components/NewsList.vue';
 // import axios from 'axios'
 export default {
-
-  data: function(){
-    return {
-      // posts: {},
-      id: this.$route.params.id,
-      menuId:'',
-      src: ''
-    }
-  },
-  methods: {
-    getAxios(){
-      this.$router.push({
-        path: "menu-detail",
-        query: { menuId: this.menuId }
-      });
+    data: function () {
+        return {
+            // posts: {},
+            id: this.$route.params.id,
+            menuId: "",
+            src: ""
+        };
     },
-    trimPostBody(body){
-      const trimBody = body.slice(0,50) + '...';
-      return trimBody
-    }
-  },
-
-  async asyncData({ query,$config,$axios }){
-    const result = await $axios.$get(
-      `${$config.apiUrl}/news/`,
-      {
-        headers: {'X-API-KEY': '691867be-4a35-4006-90c1-9b0856070900'},
-      }
-    )
-    return {
-      posts : result
-    }
-
-  }
+    methods: {
+        getAxios() {
+            this.$router.push({
+                path: "menu-detail",
+                query: { menuId: this.menuId }
+            });
+        },
+    },
+    async asyncData({ query, $config, $axios }) {
+        const result = await $axios.$get(`${$config.apiUrl}/news/`, {
+            headers: { "X-API-KEY": "691867be-4a35-4006-90c1-9b0856070900" },
+        });
+        return {
+            posts: result
+        };
+    },
+    components: { NewsList }
 }
 </script>
 
