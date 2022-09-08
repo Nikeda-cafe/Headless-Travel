fb8bd3c3c8aedcad7b7ebebbb55d756b31efc0a2
nikeda <4104cafeaulait@gmail.com>
2022-09-08 18:33:24 +0900

    20220908-5

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,76 +1,87 @@
 <template>
-    <header class="s__header w-full bg-white px-2 py-2 z-50 pb-4">
-        <div class="md:max-w-lg py-2 text-center text-lg font-bold text-gray-500">旅で人生を少し素敵に</div>
-        <div class="md:max-w-5xl mx-auto md:flex md:items-center flex">
-            <div class="w-1/2 md:w-3/5 md:flex mx-auto px-6 md:px-0 flex justify-between items-center h-16">
-                <h1>
-                    <nuxt-link
-                        to="/"
-                        class="text-black text-lg lg:text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap"
-                    >Headless Travel
+    <div>
+        <header class="s__header w-full bg-white px-2 py-2 z-50 pb-4">
+            <div class="md:max-w-lg py-2 text-center text-lg font-bold text-gray-500">旅で人生を少し素敵に</div>
+            <div class="md:max-w-5xl mx-auto md:flex md:items-center flex">
+                <div class="w-1/2 md:w-3/5 md:flex mx-auto px-6 md:px-0 flex justify-between items-center h-16">
+                    <h1>
+                        <nuxt-link
+                            to="/"
+                            class="text-center text-black text-lg lg:text-2xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap"
+                        ><p>Headless</p>
+                        <p>Travel</p>
+                        </nuxt-link>
+                    </h1>
+                </div>
+                <div class="flex justify-between items-center w-3/5">
+                    <nuxt-link to="/favorite/">
+                        <div class="texr-black md:hidden">
+                            <div class="text-center leading-none">
+                                <span class="material-icons text-2xl">
+                                    favorite_border
+                                </span>
+                            </div>
+                            <span>favarite</span>
+                        </div>
                     </nuxt-link>
-                </h1>
-            </div>
-            <div class="flex justify-between items-center w-2/5">
-                <div class="texr-black md:hidden">
-                    <span class="material-icons">
-                        favorite_border
-                    </span>
-                </div>
-                <div class="texr-black md:hidden" @click="refresh()">
-                    <span class="material-icons">
-                        restore
-                    </span>
-                </div>
-                <div class="text-black mr-4">
-                    <button class="md:hidden focus:outline-none" @click="$store.commit('toggleMenu')">
-                        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                            <path
-                                v-show="!$store.state.isMenuActive"
-                                d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
-                            />
-                            <path
-                                v-show="$store.state.isMenuActive"
-                                d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
-                            />
-                        </svg>
-                    </button>
-                    <nav
-                        class="w-full md:block absolute left-0 md:static bg-white md:bg-none z-20"
-                        :class="$store.state.isMenuActive ? 'block' : 'hidden'"
-                    >
-                        <ul class="md:flex md:justify-end md:items-end">
-                            <li class="w-full md:w-auto md:ml-5">
-                                <nuxt-link to="/news/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
-                                >記事一覧</nuxt-link>
-                            </li>
-                            <li class="w-full md:w-auto md:ml-5">
-                                <nuxt-link to="/search/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
-                                >カテゴリ一覧</nuxt-link>
-                            </li>
-                            <li class="w-full md:w-auto md:ml-5">
-                                <nuxt-link to="/contact/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
-                                >お問い合わせ</nuxt-link>
-                            </li>
-                            <li class="w-full md:w-auto md:ml-5">
-                                <a href="/information/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
-                                >お知らせ</a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <div class="texr-black md:hidden" @click="refresh()">
+                        <div class="text-center leading-none">
+                                <span class="material-icons text-2xl">
+                                    restore
+                                </span>
+                            </div>
+                            <span>histroy</span>
+                    </div>
+                    <div class="text-black mr-4">
+                        <button class="md:hidden focus:outline-none" @click="$store.commit('toggleMenu')">
+                            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                                <path
+                                    v-show="!$store.state.isMenuActive"
+                                    d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
+                                />
+                                <path
+                                    v-show="$store.state.isMenuActive"
+                                    d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
+                                />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <vue-final-modal v-model="showModal" classes="flex justify-center items-center w-full">
-            <div class="p-4 bg-white rounded text-2xl w-11/12 m-auto h-11/12">
-                <ul>
-                    <li v-for="(item,index) in viewHistory" :key="index">
-                        <span>{{item.title}}</span> : <span>{{item.id}}</span>
-                    </li>
-                </ul>
-            </div>
-        </vue-final-modal>
-    </header>
+            <vue-final-modal v-model="showModal" classes="flex justify-center items-center w-full">
+                <div class="p-4 bg-white rounded text-2xl w-11/12 m-auto h-11/12">
+                    <ul>
+                        <li v-for="(item,index) in viewHistory" :key="index">
+                            <span>{{item.title}}</span> : <span>{{item.id}}</span>
+                        </li>
+                    </ul>
+                </div>
+            </vue-final-modal>
+        </header>
+        <nav
+            class="w-full md:block absolute left-0 md:static bg-white md:bg-none z-20"
+            :class="$store.state.isMenuActive ? 'block' : 'hidden'"
+        >
+            <ul class="md:flex md:justify-end md:items-end">
+                <li class="w-full md:w-auto md:ml-5">
+                    <nuxt-link to="/news/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                    >記事一覧</nuxt-link>
+                </li>
+                <li class="w-full md:w-auto md:ml-5">
+                    <nuxt-link to="/search/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                    >カテゴリ一覧</nuxt-link>
+                </li>
+                <li class="w-full md:w-auto md:ml-5">
+                    <nuxt-link to="/contact/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                    >お問い合わせ</nuxt-link>
+                </li>
+                <li class="w-full md:w-auto md:ml-5">
+                    <a href="/information/" class="text-black md:block inline-block md:py-0 py-5 px-5 md:px-0 w-full"
+                    >お知らせ</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <script>
---
+++ pages/favorite/index.vue
@@ -0,0 +1,70 @@
+<template>
+    <section class="w-11/12 m-auto my-4">
+        <div v-if="posts">
+        <div
+            class="space-y-8 lg:divide-y lg:divide-gray-100"
+            v-for="(item, index) in posts"
+            :key="index"
+        >
+            <nuxt-link :to="`/news/${item.id}`">
+                <div class="pt-8 sm:flex lg:items-end group">
+                    <div class="flex-shrink-0 mb-4 sm:mb-0 sm:mr-4">
+                        <picture>
+                            <source :srcset="`${item.thumbnail.url}?fm=webp`" type="image/webp" />
+                            <img class="w-full rounded-md h-32 lg:w-32 object-cover" src="~/assets/midori.jpg" alt="text">
+                        </picture>
+                    </div>
+                    <div>
+                        <div class="mt-4 text-left flex items-center mb-2">
+                            <span class="material-icons text-gray-500 mr-1 text-xl">
+                                schedule
+                            </span>
+                            <span class="text-md text-gray-500" v-if="item.publishedAt">{{$dateFns.format(new Date(item.publishedAt), 'yyyy/MM/dd')}}</span>
+                        </div>
+                        <p class="mt-3 text-lg font-medium leading-6">
+                            <a
+                                href="./blog-post.html"
+                                class="text-xl text-gray-800 group-hover:text-gray-500 lg:text-2xl"
+                            >{{item.title}}
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </nuxt-link>
+        </div>
+        </div>
+        <div v-else>
+            <p>{{message}}</p>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    fetch ({store}) {
+        store.commit('resetMenu')
+    },
+    data: function(){
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    async asyncData({params, $config, $axios}) {
+        const userFavoritePosts = JSON.parse(localStorage.getItem('favo_posts')) ?? []
+        const searchPostIds = userFavoritePosts.join(',')
+        if(searchPostIds !== ''){
+            const result = await $axios.$get(`${$config.apiUrl}/news?ids=${searchPostIds}`, {
+                headers: { "X-API-KEY": "691867be-4a35-4006-90c1-9b0856070900" },
+            })
+            return {posts: result.contents}
+        }else{
+            return {message: 'お気に入り登録されている記事がありませんでした'}
+        }
+
+    },
+}
+
+</script>
