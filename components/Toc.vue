2cbd961aa4888b580e1a80fa74ba0b9afa6af741
nikeda <4104cafeaulait@gmail.com>
2022-09-19 01:57:17 +0900

    20220919-1

--- components/Toc.vue
+++ components/Toc.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="toc my-8 p-4 rounded-xl border-none shadow-lg bg-gray-50">
-        <p class="text-blue-500 font-bold text-xl mb-4">Contetns</p>
+        <p class="text-blue-500 font-bold text-xl mb-4">Contents</p>
         <li class="mb-4 text-xl" :class="`list ${item.name}`" v-for="(item,index) in toc" :key="item.id">
             <n-link v-scroll-to="`#${item.id}`" to>
                 {{index + 1}} | {{ item.text }}
