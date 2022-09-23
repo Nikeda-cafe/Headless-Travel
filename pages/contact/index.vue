ed435aadf13b7e1e4a8b72aebcf755856d36b73f
nikeda <4104cafeaulait@gmail.com>
2022-09-23 18:14:18 +0900

    mail

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -2,7 +2,7 @@
     <div class="text-center m-auto bg-gray-100 py-4">
         <PageTitle englishText="CONTACT" japaneseText="お問い合わせ" />
         <div class="w-full bg-gray-100">
-        <form class="flex w-11/12 max-w-xl space-x-3 m-auto mb-8">
+        <form class="flex w-11/12 max-w-xl space-x-3 m-auto mb-8" name="contact" netlify netlify-honeypot="bot-field" hidden>
             <div class="w-full max-w-2xl px-5 py-10 m-auto bg-white rounded-lg shadow dark:bg-gray-800">
                 <div class="mb-6 text-3xl font-light text-center text-gray-800 dark:text-white">
                     入力フォーム
@@ -12,6 +12,7 @@
                         <div class=" relative ">
                             <p class="text-left text-lg font-bold m-1">お名前*</p>
                             <input
+                                v-model="name"
                                 type="text"
                                 id="contact-form-name"
                                 class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
@@ -24,6 +25,7 @@
                         <div class=" relative ">
                             <p class="text-left text-lg font-bold m-1">メールアドレス*</p>
                             <input
+                                v-model="email"
                                 type="email"
                                 id="contact-form-email"
                                 class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
@@ -36,10 +38,10 @@
                         <p class="text-left text-lg font-bold ml-1">お問い合わせ内容*</p>
                         <label class="text-gray-700" for="name">
                             <textarea
+                                v-model="comment"
                                 class="flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-transparent"
                                 id="comment"
                                 placeholder=""
-                                name="comment"
                                 rows="5"
                                 cols="60"
                                 required
@@ -49,7 +51,7 @@
                     </div>
                     <div class="col-span-2 text-right">
                         <button
-                            type="submit"
+                            @click="submit"
                             class="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                         >
                             送信
@@ -72,9 +74,28 @@ export default {
     },
     data: function(){
         return {
-
+            name: '',
+            email: '',
+            comment: '',
+            botfield: '',
         }
     },
+    methods: {
+    async submit() {
+      const params = new FormData()
+      //以下、ダミーフォームの各フォーム要素のnameと合わせる
+      params.append('form-name', 'contact')
+      params.append('name', this.name)
+      params.append('email', this.email)
+      params.append('comment', this.comment)
+      params.append('bot-field', this.botfield)
+
+      const response = await this.$axios.$post(window.location.origin, params)
+      //実際はresponseを使って画面側にフィードバックさせるが、ここでは仮にconsoleに出力
+      window.alert('送信完了しました！')
+      console.log(response)
+    },
+  },
     fetch({ store }) {
         store.commit("resetMenu");
     },
