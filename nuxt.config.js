aded8952e891d1189c2f5bfb546e648d63a56862
nikeda <4104cafeaulait@gmail.com>
2022-09-25 14:54:24 +0900

    20220925-4

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,15 +16,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '旅行情報をお届けする情報サイト' },
       { name: 'format-detection', content: 'telephone=no' },
       { hid: "robots", name: "robots", content: "noindex" },
       { hid: 'og:site_name', property: 'og:site_name', content: 'Headless Travel' },
-      { hid: 'og:type', property: 'og:type', content: 'サイトのタイプ' },
-      { hid: 'og:url', property: 'og:url', content: 'サイトURL' },
+      { hid: 'og:url', property: 'og:url', content: 'https://headless-travel.site' },
       { hid: 'og:title', property: 'og:title', content: 'Headless Travel' },
       { hid: 'og:description', property: 'og:description', content: '海外情報サイト' },
-      { hid: 'og:image', property: 'og:image', content: 'サイトURL' },
+      { hid: 'og:image', property: 'og:image', content: 'https://headless-travel.site' },
     ],
     link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
