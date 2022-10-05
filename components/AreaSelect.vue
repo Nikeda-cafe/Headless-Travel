961df1d2cf9da536c2714a36c9613b2e4d6b98cd
nikeda <4104cafeaulait@gmail.com>
2022-10-05 23:58:19 +0900

    20221005

--- components/AreaSelect.vue
+++ components/AreaSelect.vue
@@ -5,10 +5,11 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     data: function(){
         return{
-
+            prefList: [],
         }
     },
     props: {
@@ -16,6 +17,17 @@ export default {
     },
     methods: {
 
+    },
+    async mounted() {
+        this.prefList = await axios.get('https://opendata.resas-portal.go.jp/api/v1/prefectures',
+        {
+            headers: {
+                'X-API-KEY': 'ZJ0M1oUrGqbFg8zVS1gGvYUyZm4KYm6YJTpfVeoL',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+
+        }
+        );
     },
 }
 </script>
